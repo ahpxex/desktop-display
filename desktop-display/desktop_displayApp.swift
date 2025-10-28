0beab49a21b513062e8cfb1913d7ee5244571777
破晓 <AHpx@yandex.com>
2025-10-28 12:15:16 +0800

    Initial Commit

---
+++ desktop-display/desktop_displayApp.swift
@@ -0,0 +1,17 @@
+//
+//  desktop_displayApp.swift
+//  desktop-display
+//
+//  Created by AHpx on 2025/10/28.
+//
+
+import SwiftUI
+
+@main
+struct desktop_displayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
